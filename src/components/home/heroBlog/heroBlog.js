08e1c56943947c1bdb7102488e0eee964f375014
abel-otegbola <abel15655@gmail.com>
2022-12-15 03:51:53 +0100

    fix: blog overflow in home page

--- src/components/home/heroBlog/heroBlog.js
+++ src/components/home/heroBlog/heroBlog.js
@@ -6,13 +6,13 @@ const HeroBlog = ({ blog }) => {
     const colors = ["#ff0000", "#ffa500"]
 
     return (
-        <div className="md:m-2 md:my-2 my-2 flex bg-white rounded relative">
+        <div className="md:m-2 md:my-2 my-2 sm:mb-2 mb-12 p-2 sm:flex block bg-white rounded relative">
             {
                 blog.liked ? <FaHeart  className="absolute right-5 top-5 text-red-500"/>
                 :  <FiHeart  className="absolute right-5 top-5"/>
             }
-            <img src={blog.imgUrl} className="rounded h-60 w-[40%] object-cover m-2" alt="blog" width="100%" />
-            <div className="py-3 px-2 w-[60%]">
+            <img src={blog.imgUrl} className="rounded sm:h-60 h-80 sm:w-[40%] w-[100%] object-cover" alt="blog" />
+            <div className="py-3 px-2 sm:w-[60%] w-[100%]">
                 <p className="text-fuchsia-500 py-2">{blog.createdAt}</p>
                 <h1 className="font-heading text-xl font-bold">{blog.title}</h1>
                 <p className="my-2">
