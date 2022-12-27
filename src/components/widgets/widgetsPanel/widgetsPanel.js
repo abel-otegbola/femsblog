3fae83fba170e89abaaf54d78b4d5e93e44caa55
abel-otegbola <abel15655@gmail.com>
2022-12-27 05:25:09 +0100

    fix: post page error and home page recently viewed removed

--- src/components/widgets/widgetsPanel/widgetsPanel.js
+++ src/components/widgets/widgetsPanel/widgetsPanel.js
@@ -1,27 +1,26 @@
-import { useEffect, useState } from "react";
-import RecentlyViewed from "../recentlyViewed/recentlyViewed";
+// import { useEffect, useState } from "react";
 import SavedBlogs from "../savedBlogs/savedBlogs";
 
 
 const WidgetPanel = ({ blogs }) => {
     const savedBlogs = blogs.filter(blog => blog.liked === true).splice(0, 4);
-    const [viewedIds, setViewedIds] = useState([])
-    const [recentlyViewed, setRecentlyViewed] = useState([])
+    // const [viewedIds, setViewedIds] = useState([])
+    // const [recentlyViewed, setRecentlyViewed] = useState([])
 
 
-    useEffect(() => {
-        setViewedIds([2, 1, 4])
-    }, [setViewedIds])
+    // useEffect(() => {
+    //     setViewedIds([2, 1, 4])
+    // }, [setViewedIds])
 
-    useEffect(() => {
-        let arr = viewedIds.map(id => {
-            return (
-                blogs.filter(blog => blog.id === id)
-            )
-        })
-        setRecentlyViewed([].concat(arr.map(item => { return item[0] })))
+    // useEffect(() => {
+    //     let arr = viewedIds.map(id => {
+    //         return (
+    //             blogs.filter(blog => blog.id === id)
+    //         )
+    //     })
+    //     setRecentlyViewed([].concat(arr.map(item => { return item[0] })))
 
-    }, [setRecentlyViewed, viewedIds, blogs])
+    // }, [setRecentlyViewed, viewedIds, blogs])
 
     return (
         <div>
@@ -29,7 +28,7 @@ const WidgetPanel = ({ blogs }) => {
             <button className="px-4 py-2 bg-fuchsia-600 text-white rounded my-2 hover:bg-fuchsia-700">
                 <a href="/saved">View all</a>
             </button>
-            <RecentlyViewed blogs={recentlyViewed} />
+            {/* <RecentlyViewed blogs={recentlyViewed} /> */}
         </div>
     )
 }
--- src/views/post/post.js
+++ src/views/post/post.js
@@ -29,22 +29,22 @@ const Post = () => {
                <div className="px-[3%] py-[30px]">
                 <a href={`/categories/${post.category && post.category}`} className="text-center flex w-full justify-center uppercase text-[14px] mb-3">{post.category}</a>
                     <h1 className="text-center font-bold text-2xl">{post.title}</h1>
-                    <p className="text-fuchsia-500 py-2 text-center">
+                    <p className=" py-2 text-center">
                         {
                             formatDate(post.createdAt || "2022-10-10 5:02").map((item, i) => { return (
                                 <span key={i} className="mr-1">{item}</span>
                             ) })
                         }
-                        <span className="ml-4">By {post.author}</span>
+                        <span className="ml-4 text-fuchsia-500">By {post.author}</span>
                     </p>
                     
-                    <p className="my-2 text-center">
+                    <p className="my-2 mb-4 text-center">
                         {
                         post.tags && post.tags.map((tag, i) => {
                             return <a key={i} href={`/tags/${tag.replace(" ", "-")}`} className={`py-1 px-2 pb-2 mr-2 rounded bg-fuchsia-500/[.1] text-fuchsia-600 text-sm hover:bg-fuchsia-500 hover:text-white`}>{tag}</a>
                         })
                     }</p>
-                    <img src={post.imgUrl.url && post.imgUrl.url} alt={post.title} className="md:w-[75%] w-full my-4 mx-auto" />
+                    <img src={post.imgUrl && post.imgUrl.url} alt={post.title} className="md:w-[75%] w-full my-4 mx-auto" />
 
                     <div className={`md:w-[75%] w-full mx-auto ${styles.post}`}>
                         <div className="leading-8 text-justify my-4 first-letter:text-5xl" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(post.content)}} />
