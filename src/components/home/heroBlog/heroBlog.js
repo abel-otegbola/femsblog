834e97d93657f2ea665465ea52bce3abf9bf2bf7
Abel <59369762+abel-otegbola@users.noreply.github.com>
2022-12-27 07:26:37 +0100

    Style: update the heroblog styles

--- src/components/home/heroBlog/heroBlog.js
+++ src/components/home/heroBlog/heroBlog.js
@@ -13,22 +13,23 @@ const HeroBlog = ({ blog }) => {
             }
             <img src={blog.imgUrl.url} className="rounded sm:h-auto h-auto sm:w-[40%] w-[100%] object-cover" alt={blog.imgUrl.title} />
             <div className="py-3 px-3 sm:w-[60%] w-[100%]">
-                <p className="text-gray-400 py-2">
+                <a href={`/categories/${blog.category}`} className={`py-1 px-2 mr-2 rounded text-gray-500 uppercase hover:text-fuchsia-500`}>{blog.category}</a>
+                
+                <h1 className="font-heading text-xl font-bold">{blog.title}</h1>
+                <p className="my-4">
+                    <p className="text-gray-400 py-2">
                     {
                         formatDate(blog.createdAt).map((item, i) => { return (
                             <span key={i} className="mr-1">{item}</span>
                         ) })
                     }
-                </p>
-                <h1 className="font-heading text-xl font-bold">{blog.title}</h1>
-                <p className="my-4">
-                    <a href={`/categories/${blog.category}`} className={`py-1 px-2 mr-2 rounded bg-fuchsia-100 text-fuchsia-600 text-xs hover:bg-fuchsia-500 hover:text-white`}>{blog.category}</a>
+                    </p>
                     {
                     blog.tags.map((tag, i) => {
-                        return <a href={`/tags/${tag.replace(" ", "-")}`} key={i} className={`py-1 px-2 mr-2 rounded bg-orange-100 text-red-600 text-xs hover:bg-orange-500 hover:text-white`}>{tag}</a>
+                        return <a href={`/tags/${tag.replace(" ", "-")}`} key={i} className={`py-1 px-2 mr-2 rounded uppercase text-sm hover:bg-orange-500 hover:text-white`}>{tag}</a>
                     })
                 }</p>
-                <p>{blog.summary}</p>
+                <p className="text-lg">{blog.summary}</p>
 
                 <div className="flex items-center text-gray-400 text-md mr-10 mt-5"><FiUser className="p-1 rounded-full bg-gray-300 text-white text-2xl mr-2"/>{blog.author}</div>
             </div>
